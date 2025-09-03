dfbc31a039a150a569087f800a3c83ee75ba5ad3
SaleemShahdi <shadi826@gmail.com>
2025-09-03 19:25:25 -0400

    Added classmate's code

--- detect_dups.h
+++ detect_dups.h
@@ -9,9 +9,50 @@
 
 // define the structure required to store the file paths
 
+typedef struct PathNode{
+    char *path;
+    struct PathNode *next;
+}PathNode;
+
+typedef struct SoftLinkGroup{
+    ino_t SLInode;
+    int ref_count;
+    PathNode *paths;
+    struct SoftLinkGroup *next;
+}SoftLinkGroup;
+
+typedef struct HardLinkGroup{
+    ino_t inode;
+    int ref_count;
+    PathNode *paths;
+    SoftLinkGroup *softLinks;
+    struct HardLinkGroup *next;
+}HardLinkGroup;
+
+typedef struct FileGroup{
+    int fileNum;
+    char md5str[33];
+    HardLinkGroup *hards;
+    struct FileGroup *next;
+}FileGroup;
+
+PathNode *unprocessedSL = NULL;
+
+FileGroup *FGHead = NULL;
+
 // process nftw files using this function
 static int render_file_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf);
 
 // add any other function you may need over here
-int hashFunction(const char * fpath);
-int compute_file_hash(const char *path, EVP_MD_CTX *mdctx, unsigned char *md_value, unsigned int *md5_len);
+
+char *compute_md5(const char *filename);
+
+void addUnprocessed(const char *path);
+
+FileGroup *getFG(char *md5);
+HardLinkGroup *getHL(FileGroup *current, int currentInode);
+void addPath(PathNode **head, const char* paths);
+void processUnprocessedSoftLinks();
+void printFileGroups();
+void freePaths(PathNode *head);
+void freeAll();
